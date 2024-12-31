9f9b0afb1ed57c8d3c276e8c2deadf63b518e0d4
Phan Xuan Tien <xuantiengitlab.cntt@gmail.com>
2024-12-31 19:32:49 +0700

    Blog

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -9,7 +9,7 @@ export const About = () => {
       <h2 className={styles.title}>Thông tin </h2>
       <div className={styles.content}>
         <img
-          src={getImageUrl("about/aboutImage.png")}
+          src={getImageUrl("about/anh.jpg")}
           alt="Me sitting with a laptop"
           className={styles.aboutImage}
         />
